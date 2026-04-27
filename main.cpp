4143fa3278bee154a6f7705dd91f287ce2906716
Priscilla Nowak <nowakmt@sunypoly.edu>
2026-04-27 13:59:39 -0400

    finished 1.0 of tokenizer

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 //function declerations
 vector<string> Tokenizer(string filename);
+void Cleaner(string& tempToken, vector<string>& tokens);
 
 ///////////////////////////////////////////////////////////////////////////////
 // MAIN
@@ -45,73 +46,63 @@ vector<string> Tokenizer(string filename){
             //switch case to every special character that start would mean is a new token
             switch(tempChar){
             case ' ':
-                //in every switch statement makes sure blank tokens not added to vector
-                if(!tempToken.empty()){
-                    //pushes back current token as new token is starting
-                    tokens.push_back(tempToken);
-                }
-                tempToken = "";
-                break;
-
             case '\n':
-                if(!tempToken.empty()){
-                    tokens.push_back(tempToken);
-                }
-                tempToken = "";
+                //in every switch statement makes sure blank tokens not added to vector
+                Cleaner(tempToken, tokens);
                 break;
             
             case ';':
-                if(!tempToken.empty()){
-                    tokens.push_back(tempToken);
-                }
+                Cleaner(tempToken, tokens);
                 //pushing back symbol as token as its what caused this case, used in all future statements
                 tokens.push_back(";");
-                tempToken = "";
                 break;
 
             case '+':
-                if(!tempToken.empty()){
-                    tokens.push_back(tempToken);
-
-                }
-
+                Cleaner(tempToken, tokens);
                 tokens.push_back("+");
                 tempToken = "";
                 break;
             
             case '-':
-                if(!tempToken.empty()){
-                    tokens.push_back(tempToken);
-                }
+                Cleaner(tempToken, tokens);
                 tokens.push_back("-");
-                tempToken = "";
                 break;
 
             case '*':
-                if(!tempToken.empty()){
-                    tokens.push_back(tempToken);
-                }
-
+                Cleaner(tempToken, tokens);
                 tokens.push_back("*");
-                tempToken = "";
                 break;
 
+            //come back to this later for double
             case '/':
-                if(!tempToken.empty()){
-                    tokens.push_back(tempToken);
-                }
-
+                Cleaner(tempToken, tokens);
                 tokens.push_back("/");
-                tempToken = "";
                 break;
             
+            //come back to this later for double
             case '=':
-                if(!tempToken.empty()){
-                    tokens.push_back(tempToken);
-                }
-
+                Cleaner(tempToken, tokens);
                 tokens.push_back("=");
-                tempToken = "";
+                break;
+
+            case '(':
+                Cleaner(tempToken, tokens);
+                tokens.push_back("(");
+                break;
+            
+            case ')':
+                Cleaner(tempToken, tokens);
+                tokens.push_back(")");
+                break;
+            
+            case '{':
+                Cleaner(tempToken, tokens);
+                tokens.push_back("{");
+                break;
+            
+            case '}':
+                Cleaner(tempToken, tokens);
+                tokens.push_back("}");
                 break;
             
             //if no special characters means character is part of current token
@@ -121,9 +112,7 @@ vector<string> Tokenizer(string filename){
         }
 
         //checks to see if current token is empty to make sure no accidental bloat at end of file
-        if(!tempToken.empty()){
-             tokens.push_back(tempToken);
-        }
+        Cleaner(tempToken, tokens);
 
         textfile.close();
     }
@@ -134,4 +123,19 @@ vector<string> Tokenizer(string filename){
 
     return tokens;
     
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Cleaner: helps empty tokens before pushing into vector
+///////////////////////////////////////////////////////////////////////////////
+void Cleaner(string& tempToken, vector<string>& tokens){
+    //checks if token is empty if not pushes onto stack
+    if(!tempToken.empty()){
+             tokens.push_back(tempToken);
+        }
+    
+    //starts new token by making it equal to null
+    tempToken = "";
+
+    return;
 }
