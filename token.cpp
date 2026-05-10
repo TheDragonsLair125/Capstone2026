42f35fe10973bf8cf5aab15f6e0e2c4bb252dce1
Priscilla Nowak <nowakmt@sunypoly.edu>
2026-05-10 15:16:53 -0400

    token.h made

---
+++ token.cpp
@@ -0,0 +1,25 @@
+//This file serves as the storage and constructor for the custom token class I made.
+#include<string>
+
+enum TokenType{
+    //single charcters
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    PLUS, MINUS, MULTIPLY, DIVIDE, SEMICOLON, EQUALS,
+
+    //Others
+    VAR, NUMBER, ENDFILE
+};
+
+class Token{
+    public:
+        TokenType type;
+        string value;
+        int line;
+    
+    Token(TokenType t, string v, int l){
+        type = t;
+        value = v;
+        line = l;
+    }
+        
+};
--- token.h
+++ token.h
@@ -1,24 +1,4 @@
-//This file serves as the storage and constructor for the custom token class I made.
+//token.h
 
-enum TokenType{
-    //single charcters
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    PLUS, MINUS, MULTIPLY, DIVIDE, SEMICOLON, EQUALS,
-
-    //Others
-    VAR, NUMBER, ENDFILE
-};
-
-class Token{
-    public:
-        TokenType type;
-        string value;
-        int line;
-    
-    Token(TokenType t, string v, int l){
-        type = t;
-        value = v;
-        line = l;
-    }
-        
-};
+#infdef TOKEN_H_INCLUDED_
+#define TOKEN_H_INCLUDED_
